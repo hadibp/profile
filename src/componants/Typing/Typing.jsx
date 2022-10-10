8a3c513924732d1aff24a0af1aa25cdd0232d879
ABDUL HADI BP <68989452+hadibp@users.noreply.github.com>
2022-10-10 18:14:28 +0530

    Update Typing.jsx

--- src/componants/Typing/Typing.jsx
+++ src/componants/Typing/Typing.jsx
@@ -18,7 +18,7 @@ function Typing() {
         <p>
             I am {" "}
             <Typical
-        steps={['ABDUL HADI BP', 3000, 'React Developer', 3000 ,'Student',3000 ,'Convener of TECHSIAS '  ,3000]}
+        steps={['ABDUL HADI BP', 3000, 'Flutter Developer', 3000 ,'Develop mobile apps',3000 ,'Convener of TECHSIAS'  ,3000]}
         loop={Infinity}
         wrapper="b"
       /></p>
