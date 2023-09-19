54b8088b8ec1077ac412cb947abb7b19171b2bd4
ABDUL HADI BP <68989452+hadibp@users.noreply.github.com>
2023-09-19 14:12:58 +0530

    Update Typing.jsx

--- src/componants/Typing/Typing.jsx
+++ src/componants/Typing/Typing.jsx
@@ -14,7 +14,7 @@ function Typing() {
         <p>
             I  {" "}
             <Typical
-        steps={['am ABDUL HADI BP', 3000, ' am a Flutter Developer', 3000 ,'Develop mobile apps',3000,'Develop REST FULL API in node js',3000 ,'am an ex-Convener of TECHSIAS'  ,3000,'currently working in fortlogic pvt. ltd'  ,3000]}
+        steps={['am ABDUL HADI BP', 3000, 'am a Flutter Developer', 3000, 'Develop mobile apps',3000,'Develop RESTful API in node js',3000,'am an ex-Convener of TECHSIAS' ,3000,'am Currently working in fortlogic pvt. Ltd ,3000]}
         loop={Infinity}
         wrapper="b"
       /></p>
